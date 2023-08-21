ccbda16e33ac021c8243fa4ae6ebb32a143f38a1
Misha <misha77b@gmail.com>
2023-08-21 21:50:02 +0300

    hume page some style layout

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Box } from "@mui/material";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "flex-end",
+        // background: "pink",
+        position: "relative",
+      }}
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          width: "40vw",
+          height: "98vh",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-42%, -50%)",
+          background: "pink",
+          zIndex: "-10",
+          borderRadius: "20px",
+        }}
+      ></Box>
+      <img
+        style={{ maxWidth: "700px", maxHeight: "900px" }}
+        src="./assets/home-background.png"
+        alt="cats"
+      />
+    </Box>
+  );
 };
 
 export default Home;
