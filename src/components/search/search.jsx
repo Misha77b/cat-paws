84283c7272998b2df851396bd3d98b9aea750825
Misha <misha77b@gmail.com>
2023-08-22 18:56:07 +0300

    some styling search input

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -7,18 +7,35 @@ const Search = () => {
     <Box
       sx={{
         position: "relative",
-        width: "470px",
+        maxWidth: "470px",
         // "& .MuiTextField-root": {
         //   borderRadius: "20px",
         // },
       }}
     >
-      <TextField sx={{}} placeholder="Search for breeds by name" />
+      <TextField
+        fullWidth
+        sx={{
+          "& .MuiInputBase-input": {
+            p: "15px 20px",
+            borderRadius: `20px`,
+            background: "#fff",
+          },
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderRadius: `20px`,
+              //   border: "transparent",
+            },
+          },
+        }}
+        placeholder="Search for breeds by name"
+      />
       <IconButton
         sx={{
           position: "absolute",
-          //   top: "50%",
-          right: "30px",
+          top: "50%",
+          transform: "translateY(-50%)",
+          right: "20px",
           width: "40px",
           height: "40px",
           background: "mistyRose",
