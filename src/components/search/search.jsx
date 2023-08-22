e0a354546470e11cc19f3deb6c2e8d4aed55dacc
Misha <misha77b@gmail.com>
2023-08-22 16:43:58 +0300

    search bar layout beginning

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,8 +1,38 @@
 import React from "react";
-import { Box } from "@mui/system";
+import { Box, IconButton, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Search = () => {
-  return <Box>Search</Box>;
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        width: "470px",
+        // "& .MuiTextField-root": {
+        //   borderRadius: "20px",
+        // },
+      }}
+    >
+      <TextField sx={{}} placeholder="Search for breeds by name" />
+      <IconButton
+        sx={{
+          position: "absolute",
+          //   top: "50%",
+          right: "30px",
+          width: "40px",
+          height: "40px",
+          background: "mistyRose",
+          borderRadius: "10px",
+          "&:hover": {
+            background: "mistyRose",
+          },
+        }}
+        color="primary"
+      >
+        <SearchIcon />
+      </IconButton>
+    </Box>
+  );
 };
 
 export default Search;
