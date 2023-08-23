8d5d95817235b872631d40b60c4de46c048a6bf9
Misha <misha77b@gmail.com>
2023-08-23 09:16:55 +0300

    temporry descision for header rendering

--- src/pages/favourites/Favourites.jsx
+++ src/pages/favourites/Favourites.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Header from "../../components/header/Header";
+// import Header from "../../components/header/Header";
 
 const Favourites = () => {
   return (
     <div>
       {/* for dev render header here */}
-      <Header />
+      {/* <Header /> */}
       Favourites
     </div>
   );
--- src/router/RootRouter.js
+++ src/router/RootRouter.js
@@ -2,14 +2,21 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import { Outlet } from "react-router";
 import Main from "../components/main/Main";
+import Header from "../components/header/Header";
+import { useLocation } from "react-router-dom";
 
 const RootRouter = () => {
+  console.log(useLocation().pathname);
+  //   temporary descision
+  const location = useLocation().pathname;
+
   return (
     <Grid container>
       <Grid item xs={6}>
         <Main />
       </Grid>
       <Grid item xs={6}>
+        {location === "/" ? null : <Header />}
         <Outlet />
       </Grid>
     </Grid>
