aa961f4ea1b01da11fb01db8aea79fed13ee4a5d
Misha <misha77b@gmail.com>
2023-08-31 00:00:03 +0300

    home page change path / to index:true, errorhandler page info handle using useRouteError hook and display error status, status text or message

--- src/pages/breeds/Breeds.jsx
+++ src/pages/breeds/Breeds.jsx
@@ -8,7 +8,6 @@ import GridLayout from "../../components/gridLayout/GridLayout";
 
 const Breeds = () => {
   const dispatch = useDispatch();
-  null.toString();
 
   const breeds = useSelector((state) => state.breedsReducer.breedsData);
   console.log(breeds);
