9dd28d32579583ca0daafdfe9b57b00e694f52e2
Misha <misha77b@gmail.com>
2023-10-04 23:37:50 +0300

    breed info layout beginning

--- src/pages/breedInfo/BreedInfo.jsx
+++ src/pages/breedInfo/BreedInfo.jsx
@@ -3,6 +3,11 @@ import useLocationParams from "../../hooks/useLocationParams";
 import { useParams } from "react-router-dom";
 import { fetchBreedInfo } from "../../store/reducers/breedInfoSlice";
 import { useDispatch, useSelector } from "react-redux";
+// components
+import LocationAndBackBtns from "../../components/locationAndBackBtns/LocationAndBackBtns";
+import { Box } from "@mui/material";
+import Slider from "../../components/slider/Slider";
+import BreedDescription from "./components/breedDescription/BreedDescription";
 
 const BreedInfo = () => {
   const dispatch = useDispatch();
@@ -17,7 +22,20 @@ const BreedInfo = () => {
   }, []);
   //   console.log(params.id);
   console.log("id =", id);
-  return <div>BreedInfo</div>;
+  return (
+    <Box
+      sx={{
+        width: "640px",
+        padding: "20px",
+        borderRadius: "20px",
+        background: "#FFF",
+      }}
+    >
+      <LocationAndBackBtns />
+      <Slider />
+      <BreedDescription />
+    </Box>
+  );
 };
 
 export default BreedInfo;
---
+++ src/pages/breedInfo/components/breedDescription/BreedDescription.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BreedDescription = () => {
+  return <div>BreedDescription</div>;
+};
+
+export default BreedDescription;
